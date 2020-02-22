151c44537807ef2c143ea6d1352597844c950b60
Pedro Bacchini <pedrobacchini@outlook.com>
2020-02-22 09:21:21 -0300

    Mini-Max Sum.

---
+++ mini-max-sum/src/main/java/com/github/pedrobacchini/Solution.java
@@ -0,0 +1,36 @@
+package com.github.pedrobacchini;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        long min = arr[0], max = arr[0], sum = 0;
+        for (int value : arr) {
+            sum += value;
+            if (value < min) min = value;
+            if (value > max) max = value;
+        }
+        System.out.println(String.format("%s %s", sum - max, sum - min));
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
+
